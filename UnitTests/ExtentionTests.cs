b408363c5b8d6f647cbb95b60d1aab5271977407
John Crowe <thecrow1213@gmail.com>
2013-08-02 18:37:00 -0500

    initial commit

---
+++ UnitTests/ExtentionTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ExtentionTests
+    {
+         
+    }
+}
---
+++ WithFormat/Extentions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WithFormat
+{
+    public class TryingThingsOut
+    {
+        public void TryingThisStuff()
+        {
+            const int testNum = 6;
+
+            Console.WriteLine(testNum.DecimalFormat(6));
+        }
+    }
+
+    public static class Extentions
+    {
+        public static string DecimalFormat(this int input)
+        {
+            return input.ToString(String.Format("D"));
+        }
+
+        public static string DecimalFormat(this int input, int digits)
+        {
+            return input.ToString(String.Format("D{0}", digits));
+        }
+    }
+}
