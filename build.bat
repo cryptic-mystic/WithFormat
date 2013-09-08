19f7b8b08c4bbbf338dd54da00294c7d0fb09a11
John Crowe <thecrow1213@gmail.com>
2013-09-08 15:31:03 -0500

    Adding build scripts

---
+++ build.bat
@@ -0,0 +1,3 @@
+.\.nuget\NuGet.exe install .\.nuget\packages.config -OutputDirectory .\packages
+
+.\packages\psake.4.2.0.1\tools\psake.cmd
---
+++ default.ps1
@@ -0,0 +1,17 @@
+properties {
+
+    $project = "WithFormat"
+    $configuration = 'Release'
+    $src = resolve-path '.\'
+}
+
+task default -depends Test
+
+task Test -depends Compile {
+    packages\NUnit.Runners.2.6.2\tools\nunit-console.exe UnitTests\bin\Debug\UnitTests.dll /framework=net-4.5 /nodots /xml=unittests.xml /trace=Verbose
+}
+
+task Compile {
+    msbuild /t:clean /v:q /nologo /p:Configuration=$configuration $src\$project.sln
+    msbuild /t:build /v:q /nologo /p:Configuration=$configuration $src\$project.sln
+}
