dc50409ebc6b1bbb973d46bf17352aa9e3fdf72c
John Crowe <thecrow1213@gmail.com>
2013-09-09 20:04:55 -0500

    Fixing build script to throw exception on errors

--- default.ps1
+++ default.ps1
@@ -8,10 +8,10 @@ properties {
 task default -depends Test
 
 task Test -depends Compile {
-    packages\NUnit.Runners.2.6.2\tools\nunit-console.exe UnitTests\bin\$configuration\UnitTests.dll /framework=net-4.5 /nodots /xml=unittests.xml /trace=Verbose
+    exec { packages\NUnit.Runners.2.6.2\tools\nunit-console.exe UnitTests\bin\$configuration\UnitTests.dll /framework=net-4.5 /nodots /xml=unittests.xml /trace=Verbose }
 }
 
 task Compile {
-    msbuild /t:clean /v:q /nologo /p:Configuration=$configuration $src\$project.sln
-    msbuild /t:build /v:q /nologo /p:Configuration=$configuration $src\$project.sln
+    exec { msbuild /t:clean /v:q /nologo /p:Configuration=$configuration $src\$project.sln }
+    exec { msbuild /t:build /v:q /nologo /p:Configuration=$configuration $src\$project.sln }
 }
