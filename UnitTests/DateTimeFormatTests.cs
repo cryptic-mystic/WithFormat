e60e22ce188bb440e1e0eeccde9d23625f862c34
John Crowe <thecrow1213@gmail.com>
2013-09-10 00:32:56 -0500

    Playing around with DateTime formatting. I think I have some good ideas in place...

---
+++ UnitTests/DateTimeFormatTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+using WithFormat;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DateTimeFormatTests
+    {
+        [Test]
+        public void Format_WhenInvoked_ShouldReturnFormattedStringWithDate()
+        {
+            //Arrange
+            var date = new DateTime(1988, 12, 13, 12, 1, 1, 1);
+
+            //Act
+            var result = date.WithDate().IncludeMonth().WithNumericMonth().Format();
+
+            //Assert
+            result.ShouldEqual(date.ToString("M"));
+        }
+
+        [Test]
+        public void Format_WhenInvoked_ShouldReturnFormattedStringWithYearAndMonth()
+        {
+            //Arrange
+            var date = new DateTime(1988, 12, 13, 12, 1, 1, 1);
+
+            //Act
+            var result = date.WithDate().IncludeYear().WithFourDigits().IncludeMonth().WithAbbreviatedMonth().Format();
+
+            //Assert
+            result.ShouldEqual(date.ToString("yyyy MMM"));
+        }
+    }
+}
